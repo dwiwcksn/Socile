1ab02ca6b8939ae0d5ac8edef2ad489126c5fe4a
dwiwcksn <dwi.wicaksono608@gmail.com>
2021-04-25 12:59:10 +0700

    Tujuan done

--- app/src/main/java/com/wicak/socile/ui/quiz/HasilKuis.java
+++ app/src/main/java/com/wicak/socile/ui/quiz/HasilKuis.java
@@ -29,9 +29,9 @@ public class HasilKuis extends AppCompatActivity {
         hasil.setText("Jawaban Benar : "+ MulaiKuis.benar+"\n Jawaban Salah : " + MulaiKuis.salah);
         nilai.setText(""+ MulaiKuis.hasil);
         if (MulaiKuis.hasil >= 70){
-            image.setImageResource(R.drawable.mood_up);
+            image.setImageResource(R.drawable.happy);
         }else {
-            image.setImageResource(R.drawable.mood_bad);
+            image.setImageResource(R.drawable.sad);
         }
     }
 
